f2e19122cf859ad17680c54f2fd2201ee4b6ac65
Swapnil Bhamat <bhamatswapnil@gmail.com>
2019-11-26 19:54:40 +0530

    Linting and keyword correction.

--- JS/clicker.js
+++ JS/clicker.js
@@ -1,79 +1,79 @@
 const catModel = [
   {
-    name: 'Lili',
-    url: './images/cat_1.jpg',
+    name: "Lili",
+    url: "./images/cat_1.jpg",
     touchCount: 0
   },
   {
-    name: 'Chichi',
-    url: './images/cat_2.jpg',
+    name: "Chichi",
+    url: "./images/cat_2.jpg",
     touchCount: 0
   },
   {
-    name: 'Little friend',
-    url: './images/cat_3.jpg',
+    name: "Little friend",
+    url: "./images/cat_3.jpg",
     touchCount: 0
   },
   {
-    name: 'White Angel',
-    url: './images/cat_4.jpg',
+    name: "White Angel",
+    url: "./images/cat_4.jpg",
     touchCount: 0
   },
   {
-    name: 'Cutty Cat',
-    url: './images/cat_5.jpg',
+    name: "Cutty Cat",
+    url: "./images/cat_5.jpg",
     touchCount: 0
   }
 ];
 
-let catClicker = (catData => {
+const catClicker = (catData => {
   let catCollection = [];
 
-  let preview = cat => {
+  const preview = cat => {
     let catPreview = `
       <div class="cat-box">
         <img class="cat-image" id="${cat.id}" src="${cat.url}" alt="${cat.name}" />
         <div class="cat-name top-left floating-label">${cat.name}</div>
         <div class="touch-count bottom-right floating-label" id="msg_${cat.id}">You touched ${cat.touchCount} times!!</div>
       </div>`;
-    document.getElementById('cat-preview').innerHTML = catPreview;
+    document.getElementById("cat-preview").innerHTML = catPreview;
 
     let catSelector = document.getElementById(`${cat.id}`);
-    attachPreviewListner(catSelector);
+    attachPreviewListener(catSelector);
   };
 
-  let renderList = catArr => {
-    let catList = '';
+  const renderList = catArr => {
+    let catList = "";
     for (let i = 0; i < catArr.length; i++) {
       catList += `<li class="cat" id="cat_${i}" title="${catArr[i].name}">${catArr[i].name}</li>`;
     }
-    document.getElementById('cat-list').innerHTML = catList;
-    let catSelector = document.querySelectorAll('.cat');
-    attachCatListner(catSelector);
+    document.getElementById("cat-list").innerHTML = catList;
+    let catSelector = document.querySelectorAll(".cat");
+    attachCatListener(catSelector);
   };
 
-  let attachCatListner = selector => {
+  const attachCatListener = selector => {
     selector.forEach(element => {
-      element.addEventListener('click', e => {
+      element.addEventListener("click", e => {
         let catId = parseInt(e.target.id.match(/\d+/)[0]);
         let catVO = catCollection[catId];
-        catVO['id'] = catId;
+        catVO["id"] = catId;
         preview(catVO);
       });
     });
   };
 
-  let attachPreviewListner = selector => {
-    selector.removeEventListener('click', e => {
+  const attachPreviewListener = selector => {
+    selector.removeEventListener("click", e => {
       console.log(e);
     });
-    selector.addEventListener('click', e => {
+    selector.addEventListener("click", e => {
       let catId = e.target.id;
       updateCount(catId);
     });
   };
 
-  let updateCount = id => {
+  const updateCount = id => {
     catCollection[id].touchCount++;
     document.getElementById(
       `msg_${id}`
